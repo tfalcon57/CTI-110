7ea03065e6dfca82d38ec4c46459c5e346aac366
tfalcon57 <85766421+tfalcon57@users.noreply.github.com>
2021-06-14 15:22:01 -0400

    Add files via upload
    Basic math in python.

---
+++ P1HW2_BasicMath_ FalconTristan.py
@@ -0,0 +1,41 @@
+# Displaying math in Python3
+# 06-14-2021
+# CTI-110 P1HW2 - Basic Math
+# Tristan Falcon
+# 
+
+# Create input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Display numbers entered
+print('First number entered:',num1)
+print('Second number entered:',num2)
+
+# Add the two numbers
+sum = float(num1) + float(num2)
+# Multiply the two numbers 
+product = float(num1) * float(num2)
+print()
+
+# Display the sum of the numbers entered
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# Display the product of the numbers entered
+print('The product of {0} and {1} is {2}'.format(num1, num2, product))
+
+
+# Declare Integer number1
+# Declare Integer number2
+# Declare Integer sum
+# Declare Integer product
+
+# Display "Enter first number:"
+# Input number1
+# Display "Enter second number:"
+# Input number2
+
+# Set sum = number1 = number2
+# Set product = number1 * number2
+
+# Display "The sum of", number1,"and", number2,"is: ",sum
+# Display "The product of", number1, "and", number2, "is",product
