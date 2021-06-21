003eb91f350c02fef0591172cbedddbf2966ce4b
tfalcon57 <85766421+tfalcon57@users.noreply.github.com>
2021-06-21 19:51:09 -0400

    Add files via upload
    A program that will show the distance a car has traveled.

---
+++ P2HW1_DistanceTraveled_TristanFalcon.py
@@ -0,0 +1,37 @@
+# This progarm will show the distance traveled in a car
+# 06-19-2021
+# CTI-110 P2HW1 - Distance Traveled
+# Tristan Falcon
+#
+
+# Create input numbers
+car_speed = int (input ('Enter the cars speed '))
+time_traveled = int (input ('Enter hours traveled '))
+# Set the total distance and multiply the two numbers entered by the user
+total_distance = car_speed * time_traveled
+print()
+# Display the values entered by the user 
+print ('Speed entered: ', car_speed)
+print ('Time entered: ', time_traveled)
+print()
+# Display the total distance traveled 
+print ('Distance traveled', total_distance,'miles. ')
+
+
+
+
+# Declare Integer carSpeed
+# Declare Integer timeTraveled
+# Declare Integer totalDistance
+
+# Display "Enter the cars speed "
+# Input carSpeed
+# Display "Enter hours traveled "
+# Input timeTraveled
+
+# Set totalDistance = carSpeed * timeTraveled
+
+# Display "Speed entered: "
+# Display "Time entered: "
+
+# Display "Distance traveled", totalDistance, "miles."
